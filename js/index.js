9b459534ac8f1f98ce109e6ff838f162dddc450e
Gabriel Porto Oliveira <gabrielpoliveira@ig.com.br>
2018-10-04 15:40:26 -0300

    Modificando material para alunos e começando js

--- js/index.js
+++ js/index.js
@@ -1,1 +1,16 @@
+document.getElementsByClassName("a1")[0].addEventListener("click", a1 ,true);
+document.getElementsByClassName("a2")[0].addEventListener("click", a2 ,true);
+document.getElementsByClassName("a3")[0].addEventListener("click", a3 ,true);
+document.getElementsByClassName("b1")[0].addEventListener("click", b1 ,true);
+document.getElementsByClassName("b2")[0].addEventListener("click", b2 ,true);
+document.getElementsByClassName("b3")[0].addEventListener("click", b3 ,true);
+document.getElementsByClassName("c1")[0].addEventListener("click", c1 ,true);
+document.getElementsByClassName("c2")[0].addEventListener("click", c2 ,true);
+document.getElementsByClassName("c3")[0].addEventListener("click", c3 ,true);
 
+
+
+
+function a1(){
+
+}
